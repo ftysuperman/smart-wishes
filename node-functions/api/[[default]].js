294e84b71dfa6615ec7854e95612a5cecaaf2eba
fty <fty@163.com>
2026-02-12 15:22:32 +0800

    fix: 修改路由地址

--- node-functions/api/[[default]].js
+++ node-functions/api/[[default]].js
@@ -9,13 +9,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
-
 // 路由定义
-app.use('/blessings', blessingRoutes)
+app.use('/api/blessings', blessingRoutes)
 
 app.get('/', (req, res) => {
   res.json({
